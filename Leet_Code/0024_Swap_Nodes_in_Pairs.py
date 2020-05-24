7757f96b559d260f580a12d17af659b0a3a8eb6a
Ji-In Moon <jiinmoon@protonmail.com>
2020-05-24 13:19:18 -0700

    finished 24. Swap Nodes in Pairs.

---
+++ Leet_Code/0024_Swap_Nodes_in_Pairs.py
@@ -0,0 +1,28 @@
+""" 24. Swap Nodes in Pairs
+
+Question:
+
+    Given a linked list, swap every two adjacent nodes and return its head.
+
++++
+
+Solution:
+
+    We can create as many temporary pointers as we would like - then it is all
+    about taking step-by-step, reattaching the next pointers.
+
+"""
+
+class Solution:
+    def swapPairs(self, head):
+        if not head and not head.next:
+            return head
+        dummyHead = prev = ListNode(-1)
+        dummyHead.next = head
+        while prev.next and prev.next.next:
+            A, B, C = prev.next, prev.next.next, prev.next.next.next
+            prev.next = B
+            B.next = A
+            A.next = C
+            prev = A
+        return dummyHead.next
