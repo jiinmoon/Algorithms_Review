bbb66abc5f7cecd62b9d507925bd06a47c6a33c8
Ji-In Moon <jiinmoon@protonmail.com>
2020-05-26 20:36:22 -0700

    finished 36. Valid Sudoku.

---
+++ Leet_Code/0036_Valid_Sudoku.py
@@ -0,0 +1,35 @@
+""" 36. Valid Sudoku
+
+Question:
+
+    Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be
+    validated according to the following rules, that is, rows/cols/boxes should
+    never contain duplicates from 1-9.
+
++++
+
+Solution:
+
+    The question does not ask us to check whether the given board is 'solvable'.
+    Hence, all we are required to do is check whether rules have been met -
+    thus, simply, we iterate on the Sudoku board and check.
+
+"""
+
+class Solution:
+    def isValidSudoku(self, board):
+        rows = [set() for _ in range(9)]
+        cols = [set() for _ in range(9)]
+        boxes = [set() for _ in range(9)]
+
+        for row in range(9):
+            for col in range(9):
+                num = board[row][col]
+                if num != '.':
+                    box = (row // 3) * 3 + (col // 3)
+                    if num in rows or num in cols or num in boxes:
+                        return False
+                    rows[row].add(num)
+                    cols[col].add(num)
+                    boxes[box].add(num)
+        return True
