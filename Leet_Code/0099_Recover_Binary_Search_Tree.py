9cedd38b703beda5162687e18ab58ecb64a04ff4
Ji-In Moon <jiinmoon@protonmail.com>
2020-06-01 12:40:57 -0700

    finished 99. Recover Binary Search Tree.

---
+++ Leet_Code/0099_Recover_Binary_Search_Tree.py
@@ -0,0 +1,41 @@
+""" 99. Recover Binary Search Tree
+
+Question:
+
+    Two elements of a binary search tree are swapped by a mistake - recover the
+    tree without changing the structure.
+
++++
+
+Solution:
+
+    We will simply traverse on the tree to find the two nodes which are out of
+    place, then swap their values.
+
+"""
+
+class Solution:
+    def recoverTree(self, root):
+        faultyNodes = []
+
+        # traverse and find two out of place nodes.
+        # in-order traversal on BST should be sorted order.
+        def findFaultyNodes(curr, last):
+            # continue traversing on left.
+            if curr.left:
+                last = findFaultyNodes(curr.left, last)
+            # out of order.
+            if last.val > curr.val:
+                # is it first one or second one?
+                if faultyNodes:
+                    faultyNodes[1] = curr
+                else:
+                    faultyNodes += [last, curr]
+            last = curr
+            if curr.right:
+                last = findFaultyNodes(curr.right, last)
+
+        findFaultyNodes(root, TreeNode(float('-inf')))
+        first, second = faultyNodes[0], faultyNodes[1]
+        first.val, second.val = second.val, first.val
+
