67c15b9b637ecb4c2df63ae19bae8089ebd5e4ce
Ji-In Moon <jiinmoon@protonmail.com>
2020-06-05 08:11:37 -0700

    finished 120. Triangle.

---
+++ Leet_Code/0120_Triangle.py
@@ -0,0 +1,27 @@
+""" 120. Triangle
+
+Question:
+
+    Given a triangle, find the min path sum from top to bottom.
+
++++
+
+Solution:
+
+    To compute this, we can think of this backwards - that is starting from the
+    bottom, the min path cost to reach a certain triangle[i][j] would be the
+    value itself plus the minimum value of it's two adjacent neighbours from the
+    row below (triangle[i+1][j], triangle[i+1][j+1]).
+
+"""
+
+class Solution:
+    def minimumTotal(self, triangle):
+        if not triangel:
+            return 0
+        if len(triangle):
+            for i in range(len(triangle)-2, -1, -1):
+                for j in range(len(triangle[i])):
+                    triangle[i][j] = triangle[i][j] +\
+                            min(triangle[i+1][j], triangle[i+1][j+1])
+        return triangle[0][0]
