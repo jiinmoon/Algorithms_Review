ba6014fbea1d720c9b2fed252b49a5902c637ec6
Ji-In Moon <jiinmoon@tutanota.com>
2020-07-05 12:12:52 -0700

    review - 40. Combination Sum II.

---
+++ Leet_Code/Review/0040_Combination_Sum_II.py
@@ -0,0 +1,36 @@
+""" 40. Combination Sum II
+
+Question:
+
+    Same as question 39, however, there will be duplicate elements present.
+    Find all unique set of combinations.
+
++++
+
+Solution:
+
+    In order to avoid selecting the duplicate elements in a row, we will first
+    sort the given array, and then screen out the duplicates as we select our
+    candidates for next depth.
+
+"""
+
+class Solution:
+    def combination_sum_2(self, cnadidates, target):
+        m = len(candidates)
+        result = []
+        candidates.sort()
+
+        def backtrack(start, path, pathSum):
+            if pathSum < 0:
+                return
+            if pathSum == 0:
+                result.append(path)
+                return
+            for i in range(start, m):
+                if i > 0 and candidates[i-1] == candidates[i]:
+                    continue
+                backtrack(i+1, path + [candidates[i]], pathSum - candidates[i])
+        
+        backtrack(0, [], target)
+        return result
