bbda7fa105a24ec23049f0041cb0946d441cb914
Ji-In Moon <jiinmoon@tutanota.com>
2020-07-05 11:41:25 -0700

    review - 39. Combination Sum.

---
+++ Leet_Code/Review/0039_Combination_Sum.py
@@ -0,0 +1,37 @@
+""" 39. Combination Sum
+
+Question:
+
+    Given a set of candidate numbers without duplicates, anda target number,
+    find all unique combinations in candidates where the candidate numbers sums
+    to target.
+
++++
+
+Solution:
+
+    Here, we utilize backtracking algorithm to explore all possible paths - and
+    consider the pathSum. When the current path has reached the target, then we
+    can add it to our result list to be returned.
+
+"""
+
+class Solution:
+    def combination_sum(self, candidates, target):
+        m = len(candidates)
+        result = []
+
+        def backtrack(start, path, pathSum):
+            if pathSum < 0:
+                return
+            if pathSum == 0:
+                result.append(path)
+                return
+            for i in range(start, m):
+                if candidate < pathSum:
+                    backtrack(candidates, path + [candidates[i]], pathSum
+                            - candidates[i])
+
+        backtrack(0, [], target)
+        return result
+
