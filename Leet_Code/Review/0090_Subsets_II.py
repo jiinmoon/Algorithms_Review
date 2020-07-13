939cee1fcc06366e1054ec8bcf174cafe67cdf74
Ji-In Moon <jiinmoon@tutanota.com>
2020-07-13 10:37:37 -0700

    review - 90. Subsets II.

---
+++ Leet_Code/Review/0090_Subsets_II.py
@@ -0,0 +1,29 @@
+""" 90. Subsets II
+
+Question:
+
+    Given a collection of integers that might contain duplicates, nums, return
+    all possible subsets. Solution set must not contain any duplicate subsets.
+
++++
+
+Solution:
+
+    We will start by counting the number of each element in the given list. For
+    every element, we need to append this to not only the result set but every
+    subset within the result set as well. This is where our count comes into
+    account - since the number of times that the element should appear depends
+    on the how many time it is present in the given array. 
+
+"""
+
+from collections import Counter
+
+class Solution:
+    def powerset_with_dup(self, nums):
+        result = [[]]
+        counter = Counter(nums)
+        for num in counter:
+            result += [ subset + [num] * i for i in range(1, counter[num]+1)
+                    for subset in result ]
+        return result
