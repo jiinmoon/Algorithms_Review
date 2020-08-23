d2ffcd5bb27e11668dc58454e5d3bf5c14faa782
Ji-In Moon <jiinmoon@tutanota.com>
2020-08-23 15:04:31 -0700

    added first prototype of ll in CPP

--- Data-Structures/06-LinkedList/main.cpp
+++ Data-Structures/06-LinkedList/main.cpp
@@ -17,34 +17,139 @@
  */
 
 #include <iostream>
+#include <string>
 
-struct ListNode
-{
-    int val;
-    struct ListNode *next;
-} *first = NULL;
+using std::string;
 
-void createLinkedList(const int a[], const int size)
+class ListNode
 {
-    int i;
-    struct ListNode *t, *last;
-    first = (struct ListNode*) malloc(sizeof(struct ListNode));
-    first->val = a[0];
-    first->next = NULL;
-    last = first;
-    for (i = 1; i < size; i++)
+    public:
+        int val;
+        ListNode *next;
+
+        ListNode(int data=0);
+};
+
+ListNode::ListNode(int data)
+{
+    val = data;
+    next = NULL;
+};
+
+class LinkedList
+{
+    private:
+        ListNode *head;
+        ListNode *tail;
+        int listsize;
+    public:
+        LinkedList();
+        LinkedList(const int A[], const int arraysize);
+        ~LinkedList();
+
+        void append(int val);
+        int pop();
+
+        int size();
+        string toString();
+};
+
+/* default empty constructor */
+LinkedList::LinkedList() {};
+
+/* initialize with given int array */
+LinkedList::LinkedList(const int A[], const int arraysize)
+{
+    if (arraysize != 0) 
     {
-        t = (struct ListNode*) malloc(sizeof(struct ListNode));
-        t->val = [A[i];
-        t->next = NULL;
-        last->next = t;
+        head = new ListNode(A[0]);
+        ListNode *curr = head;
+        for (int i = 1; i < arraysize; ++i)
+        {
+            curr->next = new ListNode(A[i]);
+            curr = curr->next;
+        }
+        listsize = arraysize;
     }
 }
+
+LinkedList::~LinkedList()
+{
+    while (head != NULL)
+    {
+       ListNode *t = head;
+       head = head->next;
+       delete t;
+    }
+}
+
+void LinkedList::append(int val)
+{
+    if (listsize == 0)
+    {
+        head = new ListNode(val);
+        tail = head;
+    } else {
+        tail->next = new ListNode(val);
+        tail = tail->next;
+    }
+    listsize++;
+}
+
+int LinkedList::pop()
+{
+    if (listsize > 0)
+    {
+        ListNode *curr = head;
+        while (curr->next != tail)
+            curr = curr->next;
+        ListNode *popNode = curr->next;
+        curr->next = NULL;
+        tail = curr;
+        int res = popNode->val;
+        delete popNode;
+        listsize--;
+        return res;
+    }
+    return -1;
+}
+
+string LinkedList::toString()
+{
+    string result = "";
+    ListNode *curr = head;
+    while (curr != NULL)
+    {
+        result += std::to_string(curr->val);
+        if (curr->next != NULL)
+        {
+            result += ", ";
+        }
+        curr = curr->next;
+    }
+    return result;
+}
+
+int LinkedList::size() { return listsize; }
 
 
 int main()
 {
     int a[] = {1,2,3,4,5};
+    ListNode node1;
+    ListNode node2(10); 
+    std::cout << node1.val << std::endl;
+    std::cout << node2.val << std::endl;
+    LinkedList l1(a, 5);
+    std::cout << l1.toString() << std::endl;
+    LinkedList l2;
+    l2.append(5);
+    l2.append(10);
+    l2.append(22);
+    std::cout << l2.toString() << std::endl;
+    std::cout << l2.pop() << std::endl;
+    l2.append(77);
+    std::cout << l2.toString() << std::endl;
 
     return 0;
 }
